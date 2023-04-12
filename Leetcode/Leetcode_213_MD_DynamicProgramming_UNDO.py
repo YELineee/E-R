377a4a7c54d277f714c1382f550d4c2d028b58a3
YEL!ne <1638317920@qq.com>
2023-04-12 14:50:14 +0800

    Update Leetcode_213_MD_DynamicProgramming_UNDO.py

--- Leetcode/Leetcode_213_MD_DynamicProgramming_UNDO.py
+++ Leetcode/Leetcode_213_MD_DynamicProgramming_UNDO.py
@@ -1,15 +1,22 @@
 nums = [1,2,1,1]
 
 class Solution:
-    def rob1(self, nums: list[int]) -> int:
-        f0 = f1 = 0
-        for i, x in enumerate(nums):
-            f0, f1 = f1, max(f1, f0 + x)
-        return f1
-
     def rob(self, nums: list[int]) -> int:
-        return max(nums[0] + self.rob1(nums[2:-1]), self.rob1(nums[1:]))
-
+        if len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return max(nums[0], nums[1])
+        else:
+            dp = [0] * len(nums)
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+            for i in range(2, len(nums) - 1):
+                dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+            if nums[0] > nums[1]:
+                dp[-1] = dp[-2]
+            else:
+                dp[-1] = max(dp[-2] + nums[0], nums[-1]+dp[-3]-nums[0],dp[-3])
+            return dp[-1]
         
 
 print(Solution().rob(nums))
